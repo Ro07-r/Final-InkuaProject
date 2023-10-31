cf17c2efd90b64f15362902e27e549a6b00e4e48
Ro07-r <lotierzorosalia@gmail.com>
2023-10-31 18:43:49 -0300

    Subo proyecto terminado

---
+++ client/index.js
@@ -0,0 +1,94 @@
+/*Estilos botones MP*/
+
+const productDescription = document.getElementById("product-description");
+
+const heart3Button = document.getElementById("donate-3");
+const heart5Button = document.getElementById("donate-5");
+const heart10Button = document.getElementById("donate-10");
+
+const amountInput = document.getElementById("amount-input");
+const totalAmount = document.getElementById("total-amount");
+
+let heartCount = 0;
+
+heart3Button.addEventListener("click", () => {
+  amountInput.value = 3;
+  heartCount = 3;
+  updateTotalAmount();
+});
+
+heart5Button.addEventListener("click", () => {
+  amountInput.value = 5;
+  heartCount = 5;
+  updateTotalAmount();
+});
+
+heart10Button.addEventListener("click", () => {
+  amountInput.value = 10;
+  heartCount = 10;
+  updateTotalAmount();
+});
+
+amountInput.addEventListener("input", () => {
+  heartCount = amountInput.value;
+  updateTotalAmount();
+});
+
+const updateTotalAmount = () => {
+  const updatedAmount = heartCount * 50;
+  totalAmount.innerText = updatedAmount;
+};
+
+//Mercado Pago
+const mercadopago = new MercadoPago("TEST-366f97e4-58ef-4ae9-9334-57f3015a2dd8", {
+  locale: "es-AR", // The most common are: 'pt-BR', 'es-AR' and 'en-US'
+});
+
+document.getElementById("checkout-btn").addEventListener("click", function () {
+  const orderData = {
+    quantity: 1,
+    description: productDescription.innerText,
+    price: totalAmount.innerText,
+  };
+
+  fetch("http://localhost:8080/create_preference", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(orderData),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (preference) {
+      createCheckoutButton(preference.id);
+    })
+    .catch(function () {
+      alert("Unexpected error");
+    });
+});
+
+function createCheckoutButton(preferenceId) {
+  // Initialize the checkout
+  const bricksBuilder = mercadopago.bricks();
+
+  const renderComponent = async (bricksBuilder) => {
+    if (window.checkoutButton) window.checkoutButton.unmount();
+
+    await bricksBuilder.create(
+      "wallet",
+      "button-checkout", // class/id where the payment button will be displayed
+      {
+        initialization: {
+          preferenceId: preferenceId,
+        },
+        callbacks: {
+          onError: (error) => console.error(error),
+          onReady: () => {},
+        },
+      }
+    );
+  };
+  window.checkoutButton = renderComponent(bricksBuilder);
+}
