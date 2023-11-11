8ea6f225323a38281ee107161162348205adaeac
Ro07-r <lotierzorosalia@gmail.com>
2023-11-11 18:20:23 -0300

    Subo actualizaciones al Proyecto Final

---
+++ client/index2.js
@@ -0,0 +1,44 @@
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.align-top', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.lista1', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-200px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.lista2', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-300px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.lista3', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-400px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.lista4', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-500px'
+  });
+
+
+  
